73f8292f4e296a098da6100cd30174e6b1983d9d
OWO <marco20030405@gmail.com>
2025-10-27 13:20:55 +0800

    [FE] style: Prettier code format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,23 +5,23 @@ import { setupFrontendLogger } from "@/utils/logger";
 import Main from "@/pages/Main";
 
 function App() {
-	useEffect(() => {
-		setupFrontendLogger();
-	}, []);
+  useEffect(() => {
+    setupFrontendLogger();
+  }, []);
 
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Main />} />
-				<Route path="/telemetry" element={<Main />} />
-				<Route path="/live-timing" element={<Main />} />
-				<Route path="/analytics" element={<Main />} />
-				<Route path="/standings" element={<Main />} />
-				<Route path="/schedule" element={<Main />} />
-				<Route path="/main/*" element={<Main />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/telemetry" element={<Main />} />
+        <Route path="/live-timing" element={<Main />} />
+        <Route path="/analytics" element={<Main />} />
+        <Route path="/standings" element={<Main />} />
+        <Route path="/schedule" element={<Main />} />
+        <Route path="/main/*" element={<Main />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
